ab16fb5d44e3e7806a0128f28d5867c98b0d8cd3
Mikhail Kondrashin <mkondrashin@gmail.com>
2022-05-06 19:02:09 +0300

    commit

--- dbaccess.sh
+++ dbaccess.sh
@@ -2,4 +2,4 @@
 
 
 echo curl -k --header "X-SMS-API-KEY: $(cat api_key.txt)" "https://192.168.184.102/dbAccess/tptDBServlet?method=DataDictionary&table=ACTIONSET"
-curl -k --header "X-SMS-API-KEY: $(cat api_key.txt)" "https://192.168.184.102/dbAccess/tptDBServlet?method=DataDictionary&table=ACTIONSET"
+curl -k --header "X-SMS-API-KEY: $(cat api_key.txt)" "https://192.168.184.102/dbAccess/tptDBServlet?method=DataDictionary&table=ACTIONSET&format=xml"

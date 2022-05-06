0115efe1f383bcb3554edd80a74cdc2c3cb44cf0
Mikhail Kondrashin <mkondrashin@gmail.com>
2022-05-06 16:58:25 +0300

    remove insecure TLS warning

--- main.py
+++ main.py
@@ -22,12 +22,12 @@ def main():
     n = 51
     result = sms.get_filter(settings.profile.name, 'number', n)
     filter_name = result.find('filter/name')#/filter/name").text
-    action_name = result.find('filter/actionset').name#/filter/name").text
+    action_name = result.find('filter/actionset')
     if filter_name is None or action_name is None:
         #print(result)
         #continue
         pass
-    print(filter_name.text, action_name.text)
+    print(filter_name.text, action_name.attrib['name'])
 
 if __name__ == '__main__':
     main()

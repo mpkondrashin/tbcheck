1f88c2bc71c1fa82a4fa85a4f0d8696b90dc905f
Mikhail Kondrashin <mkondrashin@gmail.com>
2022-05-06 15:34:41 +0300

    remove insecure TLS warning

--- main.py
+++ main.py
@@ -17,12 +17,14 @@ def main():
     print(settings.sms.url)
     print(settings.sms.skip_tls_verify)
     sms = SMS(settings.sms.url, settings.sms.api_key).set_insecure_skip_verify(True)
-    result = sms.get_filters('test', 'number', 51)
-    name = result.find('filter/name')#/filter/name").text
-    if name is None:
-        print("Missing name")
-        return
-    print(name.text)
+    count_missing = 0
+    for n in range(1, 100000):
+        result = sms.get_filters('test', 'number', n)
+        name = result.find('filter/name')#/filter/name").text
+        if name is None:
+            print("Missing name")
+            return
+        print(name.text)
 
 if __name__ == '__main__':
     main()

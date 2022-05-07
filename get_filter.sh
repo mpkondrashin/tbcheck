0817563931bb0588e183b5a401708dc7d9fa2e86
Mikhail Kondrashin <mkondrashin@gmail.com>
2022-05-07 09:46:05 +0300

    commit

--- get_filter.sh
+++ get_filter.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cat << EOF
+cat << EOF > getFilters.xml
 <?xml version="1.0"?>
 <getFilters>
     <profile name="tbcheck"/>
@@ -10,6 +10,6 @@ cat << EOF
         <number>3295</number>
     </filter>
 </getFilters>
-EOF > getFilters.xml
+EOF
 
 curl -X POST -k --header "X-SMS-API-KEY: $(cat api_key.txt)" --form name=@getFilters.xml https://192.168.184.102/ipsProfileMgmt/getFilters
